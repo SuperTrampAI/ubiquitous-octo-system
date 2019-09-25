f2b5a503ecf00a8af143557684548255066ed7fe
SuperTrampAI <lxh800109@gmail.com>
2019-09-25 20:42:23 +0800

    up
    up

---
+++ my-utilsuper/src/main/java/com/supertrampai/myutils/configutils/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.supertrampai.myutils.configutils;
+
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@MapperScan("com.supertramp.mapper")
+public class MybatisPlusConfig {
+
+	@Bean //分面插件
+	public PaginationInterceptor paginationInterceptor() {
+		PaginationInterceptor page = new PaginationInterceptor();
+		page.setDialectType("mysql");
+		return page;
+	}
+}
